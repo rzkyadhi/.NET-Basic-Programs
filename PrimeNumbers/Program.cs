68c79e47ecf7c9dc62392f3c5e4d95cc2e912d90
rzkyadhi <rizkyadhi081@gmail.com>
2022-06-02 10:39:15 +0700

    Prime Numbers Program

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Enter a number : ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (isPrime(number)) {
+            Console.WriteLine(number + " is a prime number");
+        } else {
+            Console.WriteLine(number + " is not a prime number");
+        }
+    }
+
+    
+    public static bool isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
