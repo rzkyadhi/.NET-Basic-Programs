7d24ad4d013233d31e7ae8605b810cfc1e370599
rzkyadhi <rizkyadhi081@gmail.com>
2022-06-05 11:57:47 +0700

    Pushin P

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace Inheritance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Modified !");
+        }
+    }
+}
